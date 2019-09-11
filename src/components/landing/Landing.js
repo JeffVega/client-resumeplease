de0d10fc876b19494faeff909f269173c9a5d552
JeffVega <jeff.vega321@gmail.com>
2019-09-11 00:32:30 -0700

    added text to landing page

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -3,10 +3,13 @@ import React from 'react'
 export default function Landing() {
     return (
         <div className="landing">
+            <div className='landing__box'>
+
+            
             <h1 className="landing__header">The Only resume you will ever need</h1>
             <p className="landing__description">Resume Please helps you build a simple, yet stunning resume for print and web </p>
             <p className="landing__nosign">No Sign-up Required</p>
-            
+            </div>
         </div>
     )
 }
