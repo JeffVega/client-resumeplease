d53347f45c17d82a2253a2a027c4cbc3119ff178
JeffVega <jeff.vega321@gmail.com>
2019-09-11 00:08:09 -0700

    installed sass for react

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 export default function Landing() {
     return (
-        <div>
+        <div className="landing">
+            <h1 className="landing__header">The Only resume you will ever need</h1>
+            <p className="landing__description">Resume Please helps you build a simple, yet stunning resume for print and web </p>
+            <p className="landing__nosign">No Sign-up Required</p>
             
         </div>
     )
